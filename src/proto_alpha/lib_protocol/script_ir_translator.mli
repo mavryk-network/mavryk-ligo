d620882e90e4a72a58d7ef157ab2baf757dfe78e
Mehdi Bouaziz <mehdi.bouaziz@nomadic-labs.com>
2021-10-13 19:28:27 +0200

    Proto: generalize location in unparse_ty

--- src/proto_alpha/lib_protocol/script_ir_translator.mli
+++ src/proto_alpha/lib_protocol/script_ir_translator.mli
@@ -318,7 +318,10 @@ val parse_ty :
   (ex_ty * context) tzresult
 
 val unparse_ty :
-  context -> 'a Script_typed_ir.ty -> (Script.node * context) tzresult
+  loc:'loc ->
+  context ->
+  'a Script_typed_ir.ty ->
+  (('loc, Script.prim) Micheline.node * context) tzresult
 
 val ty_of_comparable_ty :
   'a Script_typed_ir.comparable_ty -> 'a Script_typed_ir.ty
@@ -329,8 +332,8 @@ val parse_toplevel :
 val add_field_annot :
   Script_typed_ir.field_annot option ->
   Script_typed_ir.var_annot option ->
-  Script.node ->
-  Script.node
+  ('loc, 'prim) Micheline.node ->
+  ('loc, 'prim) Micheline.node
 
 val typecheck_code :
   legacy:bool -> context -> Script.expr -> (type_map * context) tzresult Lwt.t
