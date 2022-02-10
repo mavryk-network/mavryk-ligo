cd3c8ecf75cc37bf33bec01d342de29d9309721a
Hans Hoglund <hans.hoglund@trili.tech>
2022-02-10 11:02:20 +0000

    FIXUP: Clean up tests

--- src/proto_alpha/lib_protocol/test/unit/test_sc_rollup_storage.ml
+++ src/proto_alpha/lib_protocol/test/unit/test_sc_rollup_storage.ml
@@ -34,6 +34,17 @@
 open Protocol
 open Lwt_result_syntax
 
+(** Lift a computation using using environment errors to use shell errors. *)
+let lift k = Lwt.map Environment.wrap_tzresult k
+
+(** Trivial assertion.
+
+    By convention, context is passed linearly as [ctxt].  This takes a context
+    argument to allow this.
+    *)
+let assert_true _ctxt = return ()
+
+(** Assert that the computation fails with the given message. *)
 let assert_fails_with :
     type a.
     loc:string ->
@@ -50,17 +61,37 @@ let assert_fails_with :
       in
       Assert.equal_string ~loc expected_error_msg actual_error_msg
 
+(** Assert commitment hash equality.
+
+    By convention, context is passed linearly as [ctxt].  This takes a context
+    argument to allow this.
+    *)
+let assert_commitment_hash_equal ~loc _ctxt x y =
+  Assert.equal
+    ~loc
+    Sc_rollup_repr.Commitment_hash.equal
+    "Compare commitment hash"
+    Sc_rollup_repr.Commitment_hash.pp
+    x
+    y
+
 let new_context =
   let* (b, _contracts) = Context.init 1 in
   Incremental.begin_construction b >|=? fun inc ->
   let state = Incremental.validation_state inc in
   let ctxt = state.ctxt in
+  (* Necessary to originate rollups. *)
   let ctxt = Alpha_context.Origination_nonce.init ctxt Operation_hash.zero in
   Alpha_context.Internal_for_tests.to_raw ctxt
 
-let consume _ctxt = return ()
-
-let lift k = Lwt.map (fun x -> Environment.wrap_tzresult x) k
+let new_sc_rollup ctxt =
+  let* (rollup, _size, ctxt) =
+    Sc_rollup_storage.originate
+      ctxt
+      ~kind:Example_arith
+      ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
+  in
+  return (rollup, ctxt)
 
 let test_deposit_to_missing_rollup () =
   let* ctxt = new_context in
@@ -73,44 +104,30 @@ let test_deposit_to_missing_rollup () =
 
 let test_initial_state_is_pre_boot () =
   let* ctxt = new_context in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let* (lfc, ctxt) =
     lift @@ Sc_rollup_storage.last_final_commitment ctxt rollup
   in
-  if lfc = Sc_rollup_repr.Commitment_hash.zero then consume ctxt
-  else assert false
+  assert_commitment_hash_equal
+    ~loc:__LOC__
+    ctxt
+    lfc
+    Sc_rollup_repr.Commitment_hash.zero
 
 let test_deposit_to_existing_rollup () =
   let* ctxt = new_context in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker =
        Signature.Public_key_hash.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
      in
      let* (_, ctxt) = Sc_rollup_storage.deposit_stake ctxt rollup staker in
-     consume ctxt
+     assert_true ctxt
 
 let test_removing_staker_from_lfc_fails () =
   let* ctxt = new_context in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker =
     Signature.Public_key_hash.of_b58check_exn
       "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -124,29 +141,18 @@ let test_removing_staker_from_lfc_fails () =
 let test_deposit_then_withdraw () =
   let* ctxt = new_context in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker =
        Signature.Public_key_hash.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
      in
      let* ((), ctxt) = Sc_rollup_storage.deposit_stake ctxt rollup staker in
      let* ((), ctxt) = Sc_rollup_storage.withdraw_stake ctxt rollup staker in
-     consume ctxt
+     assert_true ctxt
 
 let test_withdraw_when_not_staked () =
   let* ctxt = new_context in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker =
     Signature.Public_key_hash.of_b58check_exn
       "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -158,13 +164,7 @@ let test_withdraw_when_not_staked () =
 
 let test_withdrawing_twice () =
   let* ctxt = new_context in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker =
     Signature.Public_key_hash.of_b58check_exn
       "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -194,12 +194,7 @@ let test_deposit_then_refine () =
   let* ctxt = new_context in
   let level = (Raw_context.current_level ctxt).level in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -218,19 +213,14 @@ let test_deposit_then_refine () =
      let* (_node, ctxt) =
        Sc_rollup_storage.refine_stake ctxt rollup level staker commitment
      in
-     consume ctxt
+     assert_true ctxt
 
 let test_finalize () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
   let level_after = Raw_level_repr.add level_0 20_160 in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -252,19 +242,13 @@ let test_finalize () =
      let* ((), ctxt) =
        Sc_rollup_storage.finalize_commitment ctxt rollup level_after c1
      in
-     consume ctxt
+     assert_true ctxt
 
 let test_finalize_fail_too_recent () =
   let* ctxt = new_context in
   let level = (Raw_context.current_level ctxt).level in
   let level_1999 = Raw_level_repr.add level 20_159 in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker =
     Sc_rollup_repr.Staker.of_b58check_exn "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
   in
@@ -296,18 +280,12 @@ let test_finalize_fail_too_recent () =
       (Sc_rollup_storage.finalize_commitment ctxt rollup level_1999 c1)
       "Attempted to finalize a commitment before its refutation deadline."
   in
-  consume ctxt
+  assert_true ctxt
 
 let test_withdrawal_fails_when_not_staked_on_lfc () =
   let* ctxt = new_context in
   let level = (Raw_context.current_level ctxt).level in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker =
     Sc_rollup_repr.Staker.of_b58check_exn "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
   in
@@ -336,12 +314,7 @@ let test_stake_on_existing_node () =
   let* ctxt = new_context in
   let level = (Raw_context.current_level ctxt).level in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker1 =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -368,19 +341,14 @@ let test_stake_on_existing_node () =
      let* (_node, ctxt) =
        Sc_rollup_storage.refine_stake ctxt rollup level staker2 commitment
      in
-     consume ctxt
+     assert_true ctxt
 
 let test_finalize_with_two_stakers () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
   let level_after = Raw_level_repr.add level_0 20_160 in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker1 =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -420,19 +388,14 @@ let test_finalize_with_two_stakers () =
      let* ((), ctxt) =
        Sc_rollup_storage.finalize_commitment ctxt rollup level_after c1
      in
-     consume ctxt
+     assert_true ctxt
 
 let test_can_remove_staker () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
   let level_after = Raw_level_repr.add level_0 20_160 in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker1 =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -473,19 +436,14 @@ let test_can_remove_staker () =
      let* ((), ctxt) =
        Sc_rollup_storage.finalize_commitment ctxt rollup level_after c1
      in
-     consume ctxt
+     assert_true ctxt
 
 let test_can_remove_staker2 () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
   let level_after = Raw_level_repr.add level_0 20_160 in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker1 =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -526,18 +484,12 @@ let test_can_remove_staker2 () =
      let* ((), ctxt) =
        Sc_rollup_storage.finalize_commitment ctxt rollup level_after c1
      in
-     consume ctxt
+     assert_true ctxt
 
 let test_removed_staker_can_not_withdraw () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker1 =
     Sc_rollup_repr.Staker.of_b58check_exn "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
   in
@@ -590,13 +542,7 @@ let test_no_finalization_on_conflict () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
   let level_after = Raw_level_repr.add level_0 2000 in
-  let* (rollup, _size, ctxt) =
-    lift
-    @@ Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-  in
+  let* (rollup, ctxt) = lift @@ new_sc_rollup ctxt in
   let staker1 =
     Sc_rollup_repr.Staker.of_b58check_exn "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
   in
@@ -646,12 +592,7 @@ let test_finds_conflict_point_at_lfc () =
   let* ctxt = new_context in
   let level_0 = (Raw_context.current_level ctxt).level in
   lift
-  @@ let* (rollup, _size, ctxt) =
-       Sc_rollup_storage.originate
-         ctxt
-         ~kind:Example_arith
-         ~boot_sector:(Sc_rollup_repr.PVM.boot_sector_of_string "")
-     in
+  @@ let* (rollup, ctxt) = new_sc_rollup ctxt in
      let staker1 =
        Sc_rollup_repr.Staker.of_b58check_exn
          "tz1SdKt9kjPp1HRQFkBmXtBhgMfvdgFhSjmG"
@@ -691,7 +632,7 @@ let test_finds_conflict_point_at_lfc () =
      let* ((left, right), ctxt) =
        Sc_rollup_storage.get_conflict_point ctxt rollup staker1 staker2
      in
-     if left = c1 && right = c2 then consume ctxt else assert false
+     if left = c1 && right = c2 then assert_true ctxt else assert false
 
 let tests =
   [
