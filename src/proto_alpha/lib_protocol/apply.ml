OKEAMAH/Git

Proto, Tx_rollup: Collect commitment bonds
Co-author-by: Marigold <contact@marigold.dev>
Co-author-by: Nomadic Labs <contact@nomadic-labs.com>
Co-author-by: Oxhead Alpha <info@oxheadalpha.com>

src/proto_alpha/lib_protocol/apply.ml
@@ -1202,19 +1202,30 @@ let apply_manager_operation_content :
       in
       return (ctxt, result, [])
   | Tx_rollup_commit {tx_rollup; commitment} -> (
-      (* TODO: bonds https://gitlab.com/tezos/tezos/-/issues/2459 *)
       match Contract.is_implicit source with
       | None ->
           fail Tx_rollup_commit_with_non_implicit_contract
           (* This is only called with implicit contracts *)
       | Some key ->
+          ( Tx_rollup_commitments.pending_bonded_commitments ctxt tx_rollup key
+          >>=? fun (ctxt, pending) ->
+            let bond_id = Rollup_bond_id.Tx_rollup_bond_id tx_rollup in
+            match pending with
+            | 0 ->
+                Token.transfer
+                  ctxt
+                  (`Contract source)
+                  (`Frozen_rollup_bonds (source, bond_id))
+                  (Constants.tx_rollup_commitment_bond ctxt)
+            | _ -> return (ctxt, []) )
+          >>=? fun (ctxt, balance_updates) ->
           Tx_rollup_commitments.add_commitment ctxt tx_rollup key commitment
           >>=? fun ctxt ->
           let result =
             Tx_rollup_commit_result
               {
                 consumed_gas = Gas.consumed ~since:before_operation ~until:ctxt;
-                balance_updates = [];
+                balance_updates;
               }
           in
           return (ctxt, result, []))

mavryk-network/mavryk-ligo

src/proto_alpha/lib_protocol/apply.ml
@@ -1274,19 +1274,34 @@ let apply_manager_operation_content :
       in
       return (ctxt, result, [])
   | Tx_rollup_commit {tx_rollup; commitment} -> (
-      (* TODO: bonds https://gitlab.com/tezos/tezos/-/issues/2459 *)
       match Contract.is_implicit source with
       | None ->
           fail Tx_rollup_commit_with_non_implicit_contract
           (* This is only called with implicit contracts *)
       | Some key ->
+          ( Tx_rollup_commitment.has_bond ctxt tx_rollup key
+          >>=? fun (ctxt, pending) ->
+            let _ = pending in
+            (* TODO/TORU: This depends on https://gitlab.com/tezos/tezos/-/merge_requests/4437
+               let bond_id = Bond_id.Tx_rollup_bond_id tx_rollup in
+               match pending with
+               | 0 ->
+                   Token.transfer
+                     ctxt
+                     (`Contract source)
+                     (`Frozen_bonds (source, bond_id))
+                     (Constants.tx_rollup_commitment_bond ctxt)
+               | _ -> return (ctxt, []) )
+            *)
+            return (ctxt, []) )
+          >>=? fun (ctxt, balance_updates) ->
           Tx_rollup_commitment.add_commitment ctxt tx_rollup key commitment
           >>=? fun ctxt ->
           let result =
             Tx_rollup_commit_result
               {
                 consumed_gas = Gas.consumed ~since:before_operation ~until:ctxt;
-                balance_updates = [];
+                balance_updates;
               }
           in
           return (ctxt, result, []))