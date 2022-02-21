2421c24a8662a2320a90521ee4ebd851dda35fea
Litchi Pi <litchi.pi@protonmail.com>
2022-02-21 16:51:09 +0100

    fixup: Fix based on comments, formatting
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- src/lib_protocol_environment/test/test_proxy_context.ml
+++ src/lib_protocol_environment/test/test_proxy_context.ml
@@ -31,134 +31,101 @@
     Subject:      Low-level operations on proxy contexts.
 *)
 
-(** Context creation *)
-
-(*
-  Genesis -- block2 -- block3a
-                  \
-                   \-- block3b
-*)
-
-let create_block ctxt =
+(* Generates data inside the context of the block *)
+let create_block (ctxt : Context.t) : Context.t Lwt.t =
   let open Lwt_syntax in
   let* ctxt = Context.add ctxt ["a"; "b"] (Bytes.of_string "Novembre") in
   let* ctxt = Context.add ctxt ["a"; "c"] (Bytes.of_string "Juin") in
   let* ctxt = Context.add ctxt ["version"] (Bytes.of_string "0.0") in
   Lwt.return ctxt
 
-type t = Context.t
+let key_to_string : String.t list -> String.t = String.concat ";"
 
-let init_contexts (f : t -> unit Lwt.t) _ () : 'a Lwt.t =
+(* Initialize the Context before starting the tests *)
+let init_contexts (f : Context.t -> unit Lwt.t) _ () : 'a Lwt.t =
   let open Lwt_syntax in
   let proxy_genesis : Context.t = Proxy_context.empty None in
   let* proxy = create_block proxy_genesis in
   f proxy
 
-let test_cmp msg testfunc val_assert proxy_ctx =
+(* Test function Context.mem *)
+let test_mem_fct (proxy : Context.t) : unit Lwt.t =
   let open Lwt_syntax in
-  (* Assert the value is the one expected *)
-  let* proxy_got = testfunc proxy_ctx in
-  Format.printf "%s: Value assertion ..." msg;
-  val_assert proxy_got ;
-  Format.printf " OK\n";
+  let assert_mem key expected =
+    let* res = Context.mem proxy key in
+    Assert.equal_bool ~msg:("Context.mem " ^ key_to_string key) expected res ;
+    Lwt.return_unit
+  in
+  let* () = assert_mem ["version"] true in
+  let* () = assert_mem ["a"; "b"] true in
+  let* () = assert_mem ["a"; "c"] true in
+  let* () = assert_mem ["a"; "d"] false in
   Lwt.return_unit
 
-(* Test MEM *)
-let test_mem_fct proxy =
+(* Test function Context.mem_tree *)
+let test_mem_tree_fct (proxy : Context.t) : unit Lwt.t =
   let open Lwt_syntax in
-  let testmemfct msg path exp =
-    test_cmp
-      msg
-      (fun ctx -> Context.mem ctx path)
-      (Assert.equal_bool ~msg exp)
-      proxy
+  let assert_mem_tree key expected =
+    let* res = Context.mem_tree proxy key in
+    Assert.equal_bool
+      ~msg:("Context.mem_tree " ^ key_to_string key)
+      expected
+      res ;
+    Lwt.return_unit
   in
-
-  let* () = testmemfct "1st_layer_leaf" ["version"] true in
-  let* () = testmemfct "2nd_layer_leaf_0" ["a"; "b"] true in
-  let* () = testmemfct "2nd_layer_leaf_1" ["a"; "c"] true in
-  let* () = testmemfct "not_exist" ["a"; "d"] false in
-
+  let* () = assert_mem_tree ["a"] true in
+  let* () = assert_mem_tree ["b"] false in
+  let* () = assert_mem_tree ["a"; "b"] false in
   Lwt.return_unit
 
-(* Test MEM TREE *)
-let test_mem_tree_fct proxy =
+(* Test function Context.find *)
+let test_find_fct (proxy : Context.t) : unit Lwt.t =
   let open Lwt_syntax in
-  let testmemtreefct msg path exp =
-    test_cmp
-      msg
-      (fun ctx -> Context.mem_tree ctx path)
-      (Assert.equal_bool ~msg exp)
-      proxy
+  let assert_find key expected =
+    let* res = Context.find proxy key in
+    Assert.equal_bytes_option
+      ~msg:("Context.find " ^ key_to_string key)
+      expected
+      res ;
+    Lwt.return_unit
   in
-
-  let* () = testmemtreefct "exist_tree" ["a"] true in
-  let* () = testmemtreefct "doesnt_exist_tree" ["b"] false in
-
-  (* let* () = testmemtreefct "is_leaf_not_tree" ["a"; "b"] false in *)
+  let* () = assert_find ["version"] (Some (Bytes.of_string "0.0")) in
+  let* () = assert_find ["a"; "b"] (Some (Bytes.of_string "Novembre")) in
+  let* () = assert_find ["a"] None in
+  let* () = assert_find ["a"; "x"] None in
   Lwt.return_unit
 
-(* Test FIND *)
-let test_find_fct proxy =
+(* Test function Context.find_tree *)
+let test_find_tree_fct (proxy : Context.t) : unit Lwt.t =
   let open Lwt_syntax in
-  let testfindfct msg path exp =
-    test_cmp
-      msg
-      (fun ctx -> Context.find ctx path)
-      (Assert.equal_bytes_option ~msg exp)
-      proxy
+  let assert_find_tree key expected =
+    let* res = Context.find_tree proxy key in
+    Assert.equal_bool
+      ~msg:("Context.find_tree " ^ key_to_string key)
+      expected
+      (Option.is_some res) ;
+    Lwt.return_unit
   in
+  let* () = assert_find_tree ["version"] true in
+  let* () = assert_find_tree ["a"; "b"] true in
+  let* () = assert_find_tree ["a"] true in
+  let* () = assert_find_tree ["a"; "x"] false in
+  Lwt.return_unit
 
+(* Test function Context.list *)
+let test_list_fct (proxy : Context.t) : unit Lwt.t =
+  let open Lwt_syntax in
+  let assert_list key fct =
+    let* res = Context.list proxy key in
+    Assert.equal_bool ~msg:("Context.list " ^ key_to_string key) true (fct res) ;
+    Lwt.return_unit
+  in
+  let* () = assert_list ["version"] (( = ) []) in
+  let* () = assert_list ["a"; "b"] (( = ) []) in
+  let* () = assert_list ["a"; "x"] (( = ) []) in
   let* () =
-    testfindfct "exist_1stlayer_leaf" ["version"] (Some (Bytes.of_string "0.0"))
+    assert_list ["a"] (fun res -> List.map (fun (i, _) -> i) res = ["b"; "c"])
   in
-  let* () =
-    testfindfct "exist_leaf" ["a"; "b"] (Some (Bytes.of_string "Novembre"))
-  in
-  let* () = testfindfct "doesnt_exist_leaf" ["a"; "x"] None in
-
-  Lwt.return_unit
-
-(* Test FIND TREE *)
-let test_find_tree_fct proxy =
-  let open Lwt_syntax in
-  let testfindtreefct msg path exp =
-    test_cmp
-      msg
-      (fun ctx -> Context.find ctx path)
-      (Assert.equal_bytes_option ~msg exp)
-      proxy
-  in
-
-  let* () =
-    testfindtreefct
-      "exist_1stlayer_leaf"
-      ["version"]
-      (Some (Bytes.of_string "0.0"))
-  in
-  let* () =
-    testfindtreefct "exist_leaf" ["a"; "b"] (Some (Bytes.of_string "Novembre"))
-  in
-  let* () = testfindtreefct "doesnt_exist_leaf" ["a"; "x"] None in
-
-  Lwt.return_unit
-
-(* Test LIST *)
-let test_list_fct proxy = 
-  let open Lwt_syntax in
-  let testlistfct msg path assert_fct =
-    test_cmp
-      msg
-      (fun ctx -> Context.list ctx path)
-      (fun ret -> Assert.equal_bool ~msg true (assert_fct ret))
-      proxy
-  in
-
-  let* () = testlistfct "exist_1stlayer_leaf" ["version"] (( = ) []) in
-  let* () = testlistfct "exist_leaf" ["a"; "b"] (( = ) []) in
-  let* () = testlistfct "doesnt_exist_leaf" ["a"; "x"] (( = ) []) in
-  let* () = testlistfct "exist_tree" ["a"] (fun res -> (List.map (fun (i, _) -> i) res) = ["b"; "c"]) in
-
   Lwt.return_unit
 
 (******************************************************************************)
@@ -170,7 +137,6 @@ let tests =
     ("find", test_find_fct);
     ("find_tree", test_find_tree_fct);
     ("list", test_list_fct);
-    (* Should break with delegation set *)
   ]
 
 let tests : unit Alcotest_lwt.test_case list =
