109aef0d546d456e16eb7427f22c968e8edd477f
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2022-11-08 00:41:26 +0530

    Win32: add missing flags for rust libs

---
+++ src/configurator/config.ml
@@ -0,0 +1,9 @@
+module C = Configurator.V1
+
+let () =
+  C.main ~name:"" (fun c ->
+      C.Flags.write_sexp
+        "win32-flags.sexp"
+        (if C.ocaml_config_var_exn (C.create "") "os_type" = "Win32"
+         then ["-cclib"; "-lole32"; "-cclib"; "-luserenv"]
+         else []))
