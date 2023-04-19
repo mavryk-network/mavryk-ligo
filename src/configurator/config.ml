35815bc5b4628fbafae568d5943df3f9af608463
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2023-04-19 18:32:57 +0530

    revert ntdll

--- src/configurator/config.ml
+++ src/configurator/config.ml
@@ -5,5 +5,5 @@ let () =
       C.Flags.write_sexp
         "win32-flags.sexp"
         (if C.ocaml_config_var_exn (C.create "") "os_type" = "Win32"
-         then ["-cclib"; "-lole32"; "-cclib"; "-luserenv"; "-cclib"; "-lbcrypt"; "-cclib"; "-lmsvcrt"; "-cclib"; "-lNtdll"]
+         then ["-cclib"; "-lole32"; "-cclib"; "-luserenv"; "-cclib"; "-lbcrypt"]
          else []))
