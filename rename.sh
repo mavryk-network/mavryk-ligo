fcaeee845287ea8ad99cba1e01061178a9a7902e
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2023-05-27 10:11:04 +0530

    *tp_environment* -> *tpenv*

--- rename.sh
+++ rename.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Get all files that match the pattern *_commands_registration.ml
-files=$(find . -type f -name "*_commands_registration.ml")
+files=$(find src -type f -name "*tp_environment*")
 
 # Rename each file
 for file in $files; do
-  new_file=${file%_commands_registration.ml}_comreg.ml
+  new_file=$(echo $file | sed s/tp_environment/tpenv/)
   mv "$file" "$new_file"
 done
